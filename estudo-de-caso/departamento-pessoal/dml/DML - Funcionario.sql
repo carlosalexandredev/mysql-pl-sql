92abc180f24c3825ca30318aa8615735f155e125
carlosalexandredev <carlosalexandredev.contato@gmail.com>
2023-07-09 10:56:23 -0300

    DML - Tabela de Funcionário | Estudo de Caso - Departamento Pessoal

---
+++ estudo-de-caso/departamento-pessoal/dml/DML - Funcionario.sql
@@ -0,0 +1,12 @@
+INSERT INTO FUNCIONARIO 
+  (ID, PRIMEIRO_NOME, SOBRENOME, NASCIMENTO, SEXO,DATA_CONTRATACAO, TIPO_CONTRATACAO) VALUES
+  (1, 'Pedro','Queiroz','2000-06-04','M','2022-09-19','CLT'),
+  (2, 'Ana','Felix','1990-03-11','F','2023-12-21','CLT'),
+  (3, 'Maiara','Brito','1997-03-15','F','2023-05-01','PJ'),
+  (4, 'Holmes','Marinho','1995-01-10','M','2023-07-23','CLT'),
+  (5, 'Sophia','Guimaraes','1992-03-08','F','2023-08-10','PJ'),
+  (6, 'Laura','Pardo','1996-03-15','F','2023-05-01','PJ'),
+  (7, 'Lucas','Silva','1995-02-10','M','2023-07-22','CLT'),
+  (8, 'Maria','Nunes','1992-03-10','F','2023-08-10','PJ'),
+  (9, 'Thainara','Souza','1992-05-11','N/A','2023-08-10','PJ'),
+  (10, 'Luana','Aguiar','2000-08-01','F','2023-08-10','PJ');
