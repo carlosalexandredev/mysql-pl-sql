d4d88818148c49a8e0d7ab6689db037850e2e688
carlosalexandredev <carlosalexandredev.contato@gmail.com>
2023-07-11 10:59:05 -0300

    DML - Tabela de Cargo | Estudo de Caso - Departamento Pessoal

---
+++ estudo-de-caso/departamento-pessoal/dml/DML - Cargo.sql
@@ -0,0 +1,7 @@
+INSERT INTO CARGO
+  (ID, CARGO, DATA_INICIO, DATA_FIM) VALUES
+  (1, 'Analista Junior', '2022-03-04', NULL),
+  (5, 'Assistente', '2022-03-04', '2024-03-04'),
+  (1, 'Analista Pleno', '2022-03-04', NULL),
+  (3, 'Assistente Pleno', '2022-03-04', '2024-03-04'),
+  (2, 'Estagiária', '2023-04-07', '2024-04-07');
