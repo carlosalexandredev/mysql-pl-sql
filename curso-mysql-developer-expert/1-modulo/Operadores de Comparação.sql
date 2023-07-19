c775f9f4fb3adf347bdf594f7739d47e82ad6b70
carlosalexandredev <carlosalexandredev.contato@gmail.com>
2023-07-19 16:42:31 -0300

    MODULO 1 - Operadores de Comparação | Curso MySQL Expert

--- curso-mysql-developer-expert/1-modulo/Operadores de Comparação.sql
+++ curso-mysql-developer-expert/1-modulo/Operadores de Comparação.sql
@@ -14,16 +14,40 @@ SELECT * FROM senso
 WHERE populacao > 100000
 AND ano = '2014';
 
+-- Operador >= (maior-igual)
+
+SELECT * FROM senso
+WHERE populacao >= 100000
+AND ano = '2014';
+
 -- Operador < (menor)
 
 SELECT * FROM senso
 WHERE populacao < 100000
 AND ano = '2014';
 
--- Operador <> (menor)
+-- Operador <= (menor-igual)
 
 SELECT * FROM senso
-WHERE cod_uf  <> '35' and cod_uf <> '14'
+WHERE populacao <= 100000
 AND ano = '2014';
+
+-- Operador <> (diferente)
+
+SELECT * FROM senso
+WHERE cod_uf  <> '35' AND cod_uf <> '14'
+AND ano = '2014';
+
+
+-- Combinando operadores
+
+SELECT * FROM senso 
+WHERE populacao <= 100000 
+	AND populacao >= 50000 
+	AND cod_uf = '35' 
+	AND nome_mun <> 'Vinhedo' 
+	AND ano = '2014';
+
+
 
 
