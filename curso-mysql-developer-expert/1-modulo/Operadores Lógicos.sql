58e54c4245a0481d1ba7bb208445b6f08f2e6427
carlosalexandredev <carlosalexandredev.contato@gmail.com>
2023-07-21 18:14:03 -0300

    MODULO 1 - Operadores de Lógicos II | Curso MySQL Expert

--- curso-mysql-developer-expert/1-modulo/Operadores Lógicos.sql
+++ curso-mysql-developer-expert/1-modulo/Operadores Lógicos.sql
@@ -58,8 +58,47 @@ AND NOT ano = '2014';
 SELECT * 
 FROM senso 
 WHERE nome_mun NOT LIKE ('A%')
-AND NOT ano = '2014';
+AND (cod_uf = '35' OR cod_uf = '15')
+AND ano = '2014';
 
+-- Operador IS NOT NULL
+SELECT * 
+FROM senso 
+WHERE (regiao IS NOT NULL OR regiao != '')
+AND (cod_uf = '35' OR cod_uf = '15')
+AND ano = '2014';
+
+-- Operador IS NULL
+SELECT * 
+FROM senso 
+WHERE (regiao IS NULL OR regiao = '')
+AND (cod_uf = '35' OR cod_uf = '15')
+AND ano = '2014';
+
+-- Preparando Cenário para IS NULL
+SELECT * 
+FROM senso 
+WHERE regiao = '';
+
+UPDATE senso 
+SET regiao = NULL 
+WHERE regiao = '';
+
+SELECT * 
+FROM senso 
+WHERE regiao IS NULL; 
+
+-- Operador HAVING
+SELECT cod_uf, estado, COUNT(*) AS quantidade  
+FROM senso
+WHERE ano = '2014'
+GROUP BY cod_uf, estado HAVING COUNT(cod_mun)>500;
+
+-- Operador HAVING
+SELECT cod_uf, estado, COUNT(*) AS quantidade  
+FROM senso
+WHERE ano = '2014'
+GROUP BY cod_uf, estado HAVING COUNT(cod_mun)<500;
 
 
 
